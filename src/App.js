dbba2e00f45f11832b39c6910b5a4b62c178f68d
Ambrose Blay <ambrose2002blay@gmail.com>
2024-01-10 21:18:48 -0500

    fix naming/importing issues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import './App.css';
-import CreatePractitioner from './createpractitioner';
+import CreatePractitioner from './CreatePractitioner';
 import IntakeForm from './IntakeForm';
 
 function App() {
