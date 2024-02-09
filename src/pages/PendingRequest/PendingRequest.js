647ca9e460964c446ef13f6be15982cf34dce701
JJBai2003 <jiayibai2003@gmail.com>
2024-02-08 20:08:40 -0500

    Added styling to pending request page

--- src/pages/PendingRequest/PendingRequest.js
+++ src/pages/PendingRequest/PendingRequest.js
@@ -11,10 +11,11 @@ function Confirmation() {
     // const params = useParams()
     return (
         <>
-            <div className="scrollable-div" style={{ overflow: "scroll" }}>
-                <h1>Pending Appointments</h1>
+            {/* <div className="scrollable-div" style={{ overflow: "scroll" }}> */}
+            <div className="scrollable-div">
+                <h3>Pending Appointments</h3>
 
-                <div class="flex-container">
+                <div class="flex-container-outer">
                     <div className='patient-details'>
                         <h3>Patient Details:</h3>
                         <div class="patient-box">
@@ -60,7 +61,7 @@ function Confirmation() {
 
                             <div className='flex-container'>
                                 <div class="p-textSpecialization">Requested Specialization:</div>
-                                <span className='textInfo'>Life Transition</span>
+                                <div className='textInfo'>Life Transition</div>
                             </div>
 
                             <div class="p-textSpecialization">This consultation request fits in your availability!</div>
@@ -163,13 +164,13 @@ function Confirmation() {
                         <div className='updateStatus'>
                             <div className='flex-container'>
                                 <h4>Update Status:</h4>
-                                <svg width="68" height="65" viewBox="0 0 68 65" fill="none" className='accept' xmlns="http://www.w3.org/2000/svg">
+                                <svg width="25" height="25" viewBox="0 0 68 65" fill="none" className='accept' xmlns="http://www.w3.org/2000/svg">
                                     <a href="/appt-approved">
                                         <path d="M34 0C27.2754 0 20.7019 1.90609 15.1106 5.47724C9.51935 9.04838 5.16149 14.1242 2.58811 20.0628C0.0147316 26.0014 -0.658582 32.536 0.653316 38.8404C1.96521 45.1448 5.2034 50.9357 9.95838 55.481C14.7134 60.0262 20.7716 63.1215 27.3669 64.3755C33.9623 65.6295 40.7986 64.9859 47.0112 62.5261C53.2239 60.0662 58.534 55.9006 62.27 50.556C66.0059 45.2114 68 38.9279 68 32.5C68 23.8805 64.4179 15.614 58.0416 9.51903C51.6654 3.4241 43.0174 0 34 0ZM29.1429 45.4768L17 33.8696L20.8614 30.1786L29.1429 38.0946L47.1386 20.8929L51.0146 24.5746L29.1429 45.4768Z" fill="#00CD81" />
                                     </a>
                                 </svg>
 
-                                <svg width="65" height="65" viewBox="0 0 65 65" fill="none" className='decline' xmlns="http://www.w3.org/2000/svg">
+                                <svg width="25" height="25" viewBox="0 0 65 65" fill="none" className='decline' xmlns="http://www.w3.org/2000/svg">
                                     <a href="/appt-declined">
                                         <path d="M32.5 64.9999C50.4497 64.9999 64.9999 50.4497 64.9999 32.5C64.9999 14.5502 50.4497 0 32.5 0C14.5502 0 0 14.5502 0 32.5C0 50.4497 14.5502 64.9999 32.5 64.9999ZM42.2499 45.4999L22.75 19.5L42.2499 45.4999ZM22.75 45.4999L42.2499 19.5L22.75 45.4999Z" fill="#EB3D25" />
                                         <g clip-path="url(#clip0_1195_502)">
