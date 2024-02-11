714013077445ca0388b617edb7ed8695ce2adaf4
Jephthah Mensah <jephthahmensah@dhcp-vl2041-37323.eduroam.cornell.edu>
2024-02-11 17:39:59 -0500

    prototype of database

--- src/CreatePractitioner.js
+++ src/CreatePractitioner.js
@@ -40,7 +40,7 @@ function CreatePractitioner() {
 
 	return (
 		<div className="App">
-			<header className="App-header">Intake Form</header>
+			<h1 className="App-header">Create Practitioner</h1>
 			<br></br>
 			<form action='/' onSubmit={handleSubmit}>
 				<input type='text' placeholder="Name" onChange={(e) => setName(e.target.value)} />
