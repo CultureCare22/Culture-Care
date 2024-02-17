c97d60402b6729ecabdf418d6f50e0c754d09768
JJBai2003 <jiayibai2003@gmail.com>
2024-02-17 14:01:51 -0500

    Added new text boxes for contact info page

--- src/pages/PatientInfoForm/PatientInfoForm.js
+++ src/pages/PatientInfoForm/PatientInfoForm.js
@@ -6,7 +6,42 @@ function Form() {
 
     return (
         <>
-        <div>Personal info form</div>
+        <div className='big-heading'>Personal info form</div>
+        <div className='container-outer'>
+            <div className='flex-row'>
+                <div className='input-field'>
+                    <div className='sub-heading'> Placeholder </div>
+                    <input type="text" placeholder="First name" />
+                </div>
+
+                <div className='input-field'>
+                    <div className='sub-heading'> Placeholder </div>
+                    <input type="text" placeholder="Last name" />
+                </div>
+            </div>
+
+            <div className='flex-row'>
+                {/* <div></div> */}
+                
+                <div className='input-field'>
+                    <div className='sub-heading'> Placeholder </div>
+                    <input type="text" placeholder="First name" />
+                </div>
+
+                <div className='input-field'>
+                    <div className='sub-heading'> Placeholder </div>
+                    <input type="text" placeholder="Last name" />
+                </div>
+            </div>
+
+            <div className='flex-row'>
+                <div className='long-input-field'>
+                    <div className='sub-heading'> Placeholder </div>
+                    <input type="text" placeholder="Location" />
+                </div>
+            </div>
+
+        </div>
         
         </>
     )
