02f101661435df40a06bdb9cdd682de0a9c6462d
JJBai2003 <jiayibai2003@gmail.com>
2024-02-19 19:55:16 -0500

    Finished Integrating Backend with Frontend

--- src/pages/PatientInfoForm/PatientInfoForm.js
+++ src/pages/PatientInfoForm/PatientInfoForm.js
@@ -1,8 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Popup from 'reactjs-popup';
 import './PatientInfoForm.css';
 
 
 function Form() {
+
+    // I am not sure if these will come in handy, but I will leave them here to save the possible work 
+    // const [firstName, setFirstName] = useState("");
+    // const [lastName, setLastName] = useState("");
+    // const [preferredName, setPreferredName] = useState("");
+    // const [address, setAddress] = useState("");
+    // const [city, setCity] = useState("");
+    const [state, setState] = useState([]);
+    // const [zipCode, setZipCode] = useState("");
+    // const [email, setEmail] = useState("");
+    // const [DoB, setDoB] = useState("");
+    // const [language, setLanguage] = useState("");
+    // const [referral, setReferral] = useState("");
+    // const [communication, setCommunication] = useState("");
+    const [payment, setPayment] = useState([]);
+    const [details, setDetails] = useState([]);
+
+    const handleSubmit = async () => {
+        // if (this.refs.required.reportValidity()) {
+        //     e.preventDefault();
+        //     // do something here
+        //   }
+        console.log("initiating submit")
+        console.log(state)
+        console.log(payment)
+        console.table(details)
+        try {
+            const response = await fetch("https://culture-care-server-a5e264f8c326.herokuapp.com/practitioners/get/1/match/", {
+                method: "POST",
+                body: JSON.stringify({
+                    data: {
+                        // firstName: firstName,
+                        // lastName: lastName,
+                        // email: email,
+                        // preferredName: preferredName,
+                        // address: address,
+                        // city: city,
+                        locations: state,
+                        // zipCode: zipCode,
+                        // DateOfBirth: DoB,
+                        // languages: language,
+                        // referral: referral,
+                        // communication: communication,
+                        paymentmethods: payment,
+                        specializations: details
+                    }
+                })
+            })
+            console.log(response)
+            if (response.ok){
+                alert("PASS")
+                window.location.href = "/clinician-s-silva"
+            }
+            else {
+                alert("Soft pass")
+                window.location.href = "/clinician-s-silva"
+            }
+        }
+        catch (error) {
+            console.log("Failed the Hard Pass")
+            alert("Sorry you are not a match! \n We are grateful for your interest in Honest Hour. It looks like our services might not be a match, but we think you could benefit from specialized support. \n Here are four websites where you can find qualified therapists in your area:")
+        }
+    }
+
+
+
 
     return (
         <>
@@ -14,11 +81,12 @@ function Form() {
                 <h2>Contact Information</h2>
                 <div className='contact-info-container'>
                     <div className='flex-row'>
+                        {/* Legal first name */}
                         <div className='input-field'>
                             <div className='sub-heading'> Legal First Name </div>
                             <input type="text" placeholder="Enter legal first name" />
                         </div>
-
+                        {/* Legal last name */}
                         <div className='input-field'>
                             <div className='sub-heading'> Legal Last Name </div>
                             <input type="text" placeholder="Enter legal last name" />
@@ -26,11 +94,13 @@ function Form() {
                     </div>
 
                     <div className='flex-row'>
+                        {/* preferred name */}
                         <div className='input-field'>
                             <div className='sub-heading'> Preferred Name </div>
                             <input type="text" placeholder="Enter preferred name" />
                         </div>
 
+                        {/* Phone number */}
                         <div className='input-field'>
                             <div className='sub-heading'> Phone Number </div>
                             <input type="text" placeholder="Enter phone number" />
@@ -38,20 +108,27 @@ function Form() {
                     </div>
 
                     <div className='flex-row'>
+                        {/* Address */}
                         <div className='long-input-field'>
                             <div className='sub-heading'>Address</div>
                             <input type="text" placeholder="Enter street address" />
                         </div>
                     </div>
                     <div className='flex-row'>
+                        {/* City */}
                         <div className='short-input-field'>
                             <div className='sub-sub-heading'> City </div>
                             <input type="text" placeholder="City" />
                         </div>
-
+                        {/* State */}
                         <div className='super-short-input-field'>
                             <div className='sub-sub-heading'> State </div>
-                            <input type="text" placeholder="State" />
+                            
+                            <input required type="text" placeholder="State" onChange={(e) => {setState([
+                            ...details,
+                            {name: e.target.value }
+                            ]);
+                        }} />
                         </div>
                         <div className='short-input-field'>
                             <div className='sub-sub-heading'> Zip Code </div>
@@ -72,7 +149,7 @@ function Form() {
 
                         <div className='input-field'>
                             <div className='sub-heading'> Language </div>
-                            <input type="text" placeholder="English or Spanish" />
+                            <input type="text" placeholder="English or Spanish"/>
                         </div>
                     </div>
                     <div className='flex-row'>
@@ -113,9 +190,13 @@ function Form() {
                             specific coverage and deductible information by filling out  
                             <a href='https://www.honesthourtherapy.com/'> this questionaire</a>
                         </p>
-                        <form className='select-fop'>
+                        <form required className='select-fop' onChange={(e) => {setPayment([
+                            ...details,
+                            {name: e.target.value }
+                            ]);
+                        }} >
                             <h3>Form of Payment</h3>
-                            <input type='radio' id='self-pay' name='payment-form' value='SELFPAY'></input>
+                            <input type='radio' id='self-pay' name='payment-form' value='SELFPAY' ></input>
                             <label for='self-pay'>Self-Pay</label><br></br>
                             <input type='radio' id='oon' name='payment-form' value='OON'></input>
                             <label for='oon'>I would like more information about OON payment</label>
@@ -125,7 +206,12 @@ function Form() {
                 </div>
                 <div className='appt-details'>
                     <h2>Appointment Details</h2>
-                    <form className='look-discuss'>
+                    <form required className='look-discuss'  onChange={(e) => {
+        setDetails([
+          ...details,
+          {name: e.target.value }
+        ]);
+      }} multiple={true} >
                         <h3>What are you looking to discuss? Select all that apply.</h3>
                         <div className='dis-options'>
                             <div className='dis-check'>
@@ -174,9 +260,12 @@ function Form() {
             </div>
         </div>
         <div className='schedule-button'>
-        <a href="/clinician-s-silva">
-            <button className='return-button'>Schedule Appointment</button> 
-        </a>
+            {/* <a href="/clinician-s-silva"> */}
+            <button className='return-button' onClick={(e) => handleSubmit(e)}>Schedule Appointment</button> 
+            {/* </a> */}
+            {/* <Popup trigger={<button className='return-button' onClick={(e) => handleSubmit(e)}>Schedule Appointment</button> } position="right center">
+                <div>Popup content here !!</div>
+            </Popup> */}
         </div>
         </>
     )
