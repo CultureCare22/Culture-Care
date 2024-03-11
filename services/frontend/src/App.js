d5ab1f8208494fa6795e4228787c8d9b31e0c093
Serena Yang <syy5@cornell.edu>
2024-03-10 23:53:09 -0400

    added individual clinician profile view

--- services/frontend/src/App.js
+++ services/frontend/src/App.js
@@ -11,6 +11,7 @@ import ApprovedApt from './pages/ApprovedApt/ApprovedApt';
 import DeclinedApt from './pages/DeclinedApt/DeclinedApt';
 import PatientInfo from './pages/PatientInfoForm/PatientInfoForm';
 import Login from './pages/Login/Login';
+import ClinicianProfile from './pages/ClinicianProfile/ClinicianProfile';
 // import PersonalInfo from './pages/PersonalInfo/PersonalInfo';
 // import Sidebar from './pages/Sidebar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -36,6 +37,7 @@ function App() {
           <Route path="/appt-declined" element={<DeclinedApt />} />
           <Route path="/appt-approved" element={<ApprovedApt />} />
           <Route path="/patient-info/:pId" element={<PatientInfo/>}/>
+          <Route path="/clinician-profile" element={<ClinicianProfile/>}/>
 
         </Routes>
         </div>
