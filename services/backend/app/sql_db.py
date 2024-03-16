a2a22f4bdd6473a0b8cf85b4c21c5351759a9980
Jephthah Mensah <jkm255@cornell.edu>
2024-03-16 19:18:33 -0400

    Removed gcal

--- services/backend/app/sql_db.py
+++ services/backend/app/sql_db.py
@@ -8,7 +8,7 @@ import bcrypt
 import datetime
 import hashlib
 import os
-from gcal_manager import appts
+# from gcal_manager import appts
 
 sql_db = SQLAlchemy()
 
@@ -200,7 +200,8 @@ class Practitioner(sql_db.Model):
         """
         Serializes a practitioner
         """
-        temp = appts()
+        # temp = appts()
+        # "appointments" : temp[self.name] if self.name in temp else []
 
         return {
             "id" : self.id,
@@ -210,8 +211,7 @@ class Practitioner(sql_db.Model):
             "languages" : [language.simple_serialize() for language in self.languages],
             "locations" : [location.simple_serialize() for location in self.locations],
             "specializations" : [specialization.simple_serialize() for specialization in self.specializations],
-            "paymentmethods" : [payment_method.simple_serialize() for payment_method in self.paymentmethods], 
-            "appointments" : temp[self.name] if self.name in temp else []
+            "paymentmethods" : [payment_method.simple_serialize() for payment_method in self.paymentmethods]
         }
 
 
